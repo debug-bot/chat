799ccd9473a4bb47cbf1206fd691143ab1fdeb49
Adnan Ashraf <adnan.newton@gmail.com>
2023-12-02 12:57:32 +0500

    changed

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Profile)
+admin.site.register(Chat)
 admin.site.register(Prompt)
 admin.site.register(Response)
+admin.site.register(File)
+admin.site.register(Settings)
+admin.site.register(Profile)
+
 
